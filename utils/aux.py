9285b17c3713fce3ff37c9b8727c80ee7851b296
carlos mazzaroli <carlitosmazaroli@gmail.com>
2023-02-15 17:36:13 -0300

    first commit

--- utils/aux.py
+++ utils/aux.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def graficar_vectores( vecs, cols, alpha=1):    
+def graficar_vectores( vecs, color, alpha=1):    
     plt.figure()
     
     plt.axvline(x=0, color='gray', zorder = 0)
@@ -16,5 +16,5 @@ def graficar_vectores( vecs, cols, alpha=1):
                    [x[2]],
                    [x[3]],
                    angles='xy', scale_units='xy', scale=1,
-                   color=cols[i],
+                   color=color[i],
                    alpha = alpha)
